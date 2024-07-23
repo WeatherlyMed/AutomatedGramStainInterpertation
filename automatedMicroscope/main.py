4234a6d4fd1c812a70849dabc647dc395c0a7621
WeatherlyMed <163483028+WeatherlyMed@users.noreply.github.com>
2024-07-22 21:27:45 -0400

    Update main.py

--- automatedMicroscope/main.py
+++ automatedMicroscope/main.py
@@ -2,47 +2,83 @@ import pygame
 import RPi.GPIO as RPIO
 from DRV8825 import DRV8825
 import time
+import os
 import pygame.camera
 
-steplang = 3
+slide_number = "1" 
+dimension = 5 
 
-def take_and_display(camera):
-    # Capture image
+if not isinstance(dimension, int):
+    print("Improper dimension")
+    exit(1)
+
+dr = "/media/pi/PortableSSD/" 
+dirname = "Slide" + slide_number
+try:
+    path = os.path.join(dr, dirname)
+    os.mkdir(path)
+except FileExistsError:
+    print("Slide has been processed")
+    exit(1)
+
+steplang = 2500
+
+def take(camera, a, b):
     pic = camera.get_image()
-    # Display image on screen
-    screen.blit(pic, (0, 0))
-    pygame.display.update()
+    pygame.image.save(pic, fileName(a, b))
 
-pygame.init()
-pygame.camera.init()
+def fileName(a, b):
+    num = str((a * 100) + b)
+    name = os.path.join(dr, dirname, 'ISR' + num + '.png')
+    return name
 
-# Set up display
-screen = pygame.display.set_mode((640, 480))
-
-# Get camera list and start camera
-camlist = pygame.camera.list_cameras()
-if camlist:
-  cam = pygame.camera.Camera(camlist[0], (640, 480))
-else:
-  print("Camera Failed to Open")
-  exit(1)
-cam.start()
-Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, modepins=(16, 17, 20))
-Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, modepins=(21, 22, 27))
-Motor1.SetMicroStep('softward', '1/32step')
-Motor2.SetMicroStep('softward', '1/32step')
-i, j = 32, 32  # Set the dimensions of the photo grid
-for x in range(i):
-  for y in range(j):
-    take_and_display(cam)
-    time.sleep(0.05)
+def moveRight():
     Motor2.TurnStep(Dir='forward', steps=steplang, stepdelay=0.00)
     Motor2.Stop()
-  Motor1.TurnStep(Dir='forward', steps=steplang, stepdelay=0.00)
-  Motor1.Stop()
-##End Process
-RPIO.cleanup()
+
+def moveLeft():
+    Motor2.TurnStep(Dir='backward', steps=steplang, stepdelay=0.00)
+    Motor2.Stop()
+
+def moveDown():
+    Motor1.TurnStep(Dir='forward', steps=steplang, stepdelay=0.00)
+    Motor1.Stop()
+
+def moveUp():
+    Motor1.TurnStep(Dir='backward', steps=steplang, stepdelay=0.0)
+    Motor1.Stop()
+
+pygame.camera.init()
+camlist = pygame.camera.list_cameras()
+if camlist:
+    cam = pygame.camera.Camera(camlist[0], (1920, 1080))
+    cam.start()
+else:
+    print("\error cam")
+    exit(1)
+
+try:
+    Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+    Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+    Motor1.SetMicroStep('software', '1/32step')
+    Motor2.SetMicroStep('software', '1/32step')
+except Exception as e:
+    RPIO.cleanup()
+    print("\nFailure Mounting:", e)
+    cam.stop()
+    exit(1)
+
+for i in range(dimension):
+    for k in range(dimension):
+        moveDown()
+        time.sleep(0.5)
+        take(cam, i, k)
+    moveUp()
+    moveRight()
+moveDown()
+
+## end protocol
 Motor1.Stop()
 Motor2.Stop()
 cam.stop()
-pygame.quit()
+RPIO.cleanup()
